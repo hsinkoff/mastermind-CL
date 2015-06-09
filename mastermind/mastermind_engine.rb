db11f35737ec86946afb4ca60ef269b58c317538
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-06-09 15:50:21 +0000

    combine two methods

--- mastermind/mastermind_engine.rb
+++ mastermind/mastermind_engine.rb
@@ -77,7 +77,7 @@ class MastermindEngine
   # moves known color to next position if necessary
   def reorder
     if @correct_color.length >= 2
-      if @correct_color[@correct_color.length - 1] != 0
+      if @correct_color[@correct_color.length - 1] != "0"
         if (@partial_guess.index(@color_array[0]).to_i + 1) <= (3 - @final_guess.compact.length.to_i) && @final_guess[(@partial_guess.index(@color_array[0]).to_i + 1)] == nil
           @partial_guess[(@partial_guess.index(@color_array[0]).to_i + 1)] = @color_array[0]
           @partial_guess[@partial_guess.index(@color_array[0]).to_i] = nil 
@@ -90,10 +90,23 @@ class MastermindEngine
     end 
   end
 
-  # fills in partial_guess with next color
-  # adds partial_guess onto the end of guess and clears partial_guess
-  def color_fill
-    @partial_guess.each_index do |index|
+  # fills in partial_guess with final_guess
+  # fills in blanks with next color
+  def final_round
+    if @final_guess.compact.length == 4
+      @guess << @final_guess
+      return
+    elsif @final_guess.compact.length >= 2 && @correct_color[@correct_color.length - 1].to_i == 1
+      @partial_guess << @final_guess[0] << @final_guess[1] << @final_guess[2] << @final_guess[3]
+      @partial_guess.each_index do |index|
+        if @partial_guess[index] == nil
+          @partial_guess[index] = @color_array.shift
+          @color_array << (@partial_guess[index])
+        end
+      end
+      @color_array.rotate!
+    end
+     @partial_guess.each_index do |index|
       if @partial_guess[index] == nil
         if @color_array.length.to_i + @final_guess.compact.length.to_i == 4
           @partial_guess[index] = ((@colors - @color_array) - @final_guess)[-1]
@@ -106,31 +119,12 @@ class MastermindEngine
     @partial_guess = []
   end
 
-  # runs if game is near the end
-  # fills in partial_guess with final_guess
-  def final_round
-    if @final_guess.compact.length == 4
-      @guess << @final_guess
-    elsif @final_guess.compact.length >= 2 && @correct_color[@correct_color.length - 1].to_i >= 1
-      @partial_guess << @final_guess[0] << @final_guess[1] << @final_guess[2] << @final_guess[3]
-      @partial_guess.each_index do |index|
-        if @partial_guess[index] == nil
-          @partial_guess[index] = @color_array.shift
-          @color_array << (@partial_guess[index])
-        end
-      end
-      @color_array.rotate!
-      @guess << @partial_guess
-    end
-  end
-
   # runs through methods once
   def complete_round
     self.correct_places
     self.color_check
     self.fill_in_known
     self.reorder
-    self.color_fill
     self.final_round
   end
 end
