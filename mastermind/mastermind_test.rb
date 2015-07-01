cdb44d1762ae37be5d35efe913aef297948c51ca
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-07-01 23:34:43 +0000

    initial testing

---
+++ mastermind/mastermind_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require_relative 'mastermind_engine'
+
+class TestMastermind < Minitest::Test
+	def test_check
+		assert_equal 1, 1
+	end
+
+	def test_initialize
+		game = MastermindEngine.new([0], [0], [["red", "red", "red", "red"]])
+		assert_equal [0], game.correct_color
+		assert_equal [0], game.correct_place
+		assert_equal [["red", "red", "red", "red"]], game.guess
+	end
+
+	def test_correctplaces
+		game = MastermindEngine.new([0], [0], [["red", "red", "red", "red"]])
+		game.correct_places
+		assert_equal [["red", "red", "red", "red"]], game.guess
+	end
+end
