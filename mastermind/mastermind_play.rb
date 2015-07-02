371fe927e5feda5e5759b850f41ac618fb4778cd
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-07-02 21:12:27 +0000

    update method responsibility

--- mastermind/mastermind_play.rb
+++ mastermind/mastermind_play.rb
@@ -2,17 +2,19 @@ require "./mastermind_engine.rb"
 require "./mastermind_run.rb"
 
 class Game
-  # sets up and runs one complete game
+  def initialize
+    @run = MastermindRun.new
+    @engine = MastermindEngine.new(@run.correct_color, @run.correct_place, @run.guess)
+  end
+  
   def play
-    run = MastermindRun.new
-    engine = MastermindEngine.new(run.correct_color, run.correct_place, run.guess)
-    run.welcome
+    @run.welcome
     10.times do
-      run.round
-      engine.complete_round
+      @run.round
+      @engine.complete_round
     end
-    run.game_over
-    run.play_another_game
+    @run.game_over
+    @run.play_another_game
   end
 end
 
