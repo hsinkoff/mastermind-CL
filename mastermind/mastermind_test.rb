b644376564d030f94f6f508d18cf6063afa6bdf7
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-07-07 00:03:14 +0000

    initial tests

--- mastermind/mastermind_test.rb
+++ mastermind/mastermind_test.rb
@@ -1,7 +1,8 @@
 require 'minitest/autorun'
-require_relative 'mastermind_engine'
+require_relative './mastermind_engine'
 
 class TestMastermind < Minitest::Test
+  
   def test_check
     assert_equal 1, 1
   end
@@ -13,9 +14,31 @@ class TestMastermind < Minitest::Test
     assert_equal [["red", "red", "red", "red"]], game.guess
   end
 
-  def test_correctplaces
+  def test_complete_round
     game = MastermindEngine.new([0], [0], [["red", "red", "red", "red"]])
-    game.correct_places
-    assert_equal [["red", "red", "red", "red"]], game.guess
+    game.complete_round
+    assert_equal [], game.instance_variable_get(:@color_array)
+    assert_equal ["green", "green", "green", "green"], game.guess[-1]
+   
+    game1 = MastermindEngine.new([1], [0], [["red", "red", "red", "red"]])
+    game1.complete_round
+    assert_equal ["red"], game1.instance_variable_get(:@color_array)
+    assert_equal ["red", "green", "green", "green"], game1.guess[-1]
+
+    game2 = MastermindEngine.new([2], [0], [["red", "red", "red", "red"]])
+    game2.complete_round
+    assert_equal ["red", "green", "green", "green"], game2.guess[-1]
+    assert_equal ["red", "red"], game2.instance_variable_get(:@color_array)
+
+    game3 = MastermindEngine.new([3], [0], [["red", "red", "red", "red"]])
+    game3.complete_round
+    assert_equal ["red", "green", "green", "green"], game3.guess[-1]
+    assert_equal ["red", "red", "red"], game3.instance_variable_get(:@color_array)
+    
+    game4 = MastermindEngine.new([4], [0], [["red", "red", "red", "red"]])
+    game4.complete_round
+    assert_equal ["red", "purple", "purple", "purple"], game4.guess[-1]
+    assert_equal ["red", "red", "red", "red"], game4.instance_variable_get(:@color_array)
+
   end
 end
