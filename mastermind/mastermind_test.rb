1573e4a4f411579a1d4354f4da6397f63d827aef
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-07-10 22:42:30 +0000

    add tests for Color class

--- mastermind/mastermind_test.rb
+++ mastermind/mastermind_test.rb
@@ -7,38 +7,43 @@ class TestMastermind < Minitest::Test
     assert_equal 1, 1
   end
 
-  def test_initialize
-    game = MastermindEngine.new([0], [0], [["red", "red", "red", "red"]])
-    assert_equal [0], game.correct_color
-    assert_equal [0], game.correct_place
-    assert_equal [["red", "red", "red", "red"]], game.guess
+  def test_determine_colors_in_answer
+    @color = MastermindColor.new([0], [0], [["red", "red", "red", "red"]])
+    @color.add_colors_to_color_array
+    assert_equal [], @color.color_array
+    
+    @color.correct_color << 1
+    @color.correct_place << 0
+    @color.add_colors_to_color_array
+    assert_equal ["green"], @color.color_array
+    
+    @color.correct_color << 2
+    @color.correct_place << 0
+    @color.add_colors_to_color_array
+    assert_equal ["green", "orange", "orange"], @color.color_array
+    
+    @color.final_guess = ["green"]
+    @color.color_array = ["orange", "orange"]
+    @color.correct_color << 3
+    @color.correct_place << 0
+    @color.add_colors_to_color_array
+    assert_equal ["orange", "orange", "yellow"], @color.color_array
+
+    @color1 = MastermindColor.new([0], [1], [["red", "red", "red", "red"]])
+    @color1.add_colors_to_color_array
+    assert_equal ["red"], @color1.color_array
+    @color1.correct_color << 1
+    @color1.correct_place << 1
+    @color1.add_colors_to_color_array
+    assert_equal ["red", "green", "green"], @color1.color_array
   end
 
-  def test_complete_round
-    game = MastermindEngine.new([0], [0], [["red", "red", "red", "red"]])
-    game.complete_round
-    assert_equal [], game.instance_variable_get(:@color_array)
-    assert_equal ["green", "green", "green", "green"], game.guess[-1]
-   
-    game1 = MastermindEngine.new([1], [0], [["red", "red", "red", "red"]])
-    game1.complete_round
-    assert_equal ["red"], game1.instance_variable_get(:@color_array)
-    assert_equal ["red", "green", "green", "green"], game1.guess[-1]
+  def test_place_color
 
-    game2 = MastermindEngine.new([2], [0], [["red", "red", "red", "red"]])
-    game2.complete_round
-    assert_equal ["red", "green", "green", "green"], game2.guess[-1]
-    assert_equal ["red", "red"], game2.instance_variable_get(:@color_array)
 
-    game3 = MastermindEngine.new([3], [0], [["red", "red", "red", "red"]])
-    game3.complete_round
-    assert_equal ["red", "green", "green", "green"], game3.guess[-1]
-    assert_equal ["red", "red", "red"], game3.instance_variable_get(:@color_array)
-    
-    game4 = MastermindEngine.new([4], [0], [["red", "red", "red", "red"]])
-    game4.complete_round
-    assert_equal ["red", "purple", "purple", "purple"], game4.guess[-1]
-    assert_equal ["red", "red", "red", "red"], game4.instance_variable_get(:@color_array)
+  end
+
+  def test_guess_for_this_turn
 
   end
 end
